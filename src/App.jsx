feeb620110b7de1f6aaaf45a80efff48acfe6778
nicholasb4711 <byrne.ni@northeastern.edu>
2024-05-16 15:42:53 -0400

    beginning modal setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react'
 import ContactList from './ContactList'
 import ContactForm from './ContactForm'
 import './App.css'
+import { is } from 'express/lib/request'
 
 function App() {
   const [contacts, setContacts] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   useEffect(() => {
     fetchContacts()
@@ -17,9 +19,19 @@ function App() {
     console.log(data.contacts)
   }
 
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
   return (
     <>
       <ContactList contacts={contacts} />
+      {isModalOpen && <div>
+        </div>}
       <ContactForm />
     </>
   )
